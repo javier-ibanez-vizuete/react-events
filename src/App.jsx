d6d91e3d15a8223ba1827d69ea2263907748b630
Javier Ibañez Vizuete <javieriibanezvizuete@gmail.com>
2025-07-04 12:03:32 +0200

    feat(app): add a input with a real time display value

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { ClicksCounter } from "./components/ClicksCounter/ClicksCounter";
+import { InputMirror } from "./components/InputMirror/InputMirror";
 
 export const App = () => {
 	return (
 		<>
 			<h1>Ejercicio eventos</h1>
 			<ClicksCounter />
+			<InputMirror />
 		</>
 	);
 };
